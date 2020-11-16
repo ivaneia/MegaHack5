66850f1027f0da8ce29c049d9812378066eee984
Ivaneia <74514418+ivaneia@users.noreply.github.com>
2020-11-15 22:37:26 -0300

    PREVER CEO

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# MegaHack5
-Hackathon
+
+# PREVER CEO
+## Mega Hack 5 Edição
+
+Um projeto de Hackathon no Kaggle https://www.kaggle.com/ivaneia/prever-ceo
+
+Os campos do conjunto de dados incluem:
+
+
+* Número de projetos
+* Tempo passado na empresa
+* Se eles tiveram uma promoção nos últimos 5 anos
+* Departamento
+* Salário
+* Se o funcionário saiu
+
+
+O conjunto de dados original é armazenado na pasta 'Original Kaggle Dataset'. Os dados limpos e o código são armazenados na pasta 'cleaned data'.
+
+Grupo 31 - Desafio Velt
+
---
+++ code/data_preparation.py
@@ -0,0 +1,36 @@
+
+# import dependecies
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+#% matplotlib inline
+
+df = pd.read_csv(path + "data_original.csv")
+
+
+sales_dummies = pd.get_dummies(df.sales, prefix="sales_").astype("int")
+salary_dummies = pd.get_dummies(df.salary, prefix="salary_").astype("int")
+
+data = pd.concat([sales_dummies, salary_dummies, df.drop("left", axis=1)], axis=1).drop(["sales", "salary"], axis=1)
+data = pd.concat([data, df.left], axis=1)
+
+
+#PLOTS
+# Correlation Heatmap
+cor = data.corr()
+mask = np.zeros_like(cor)
+mask[np.triu_indices_from(mask)] = True
+with sns.axes_style("white"):
+    ax = sns.heatmap(cor, mask=mask, vmax=.3, square=True)
+plt.show()
+
+g = sns.factorplot(x="satisfaction_level", y="promotion_last_5years",
+                   hue="left", row="salary",
+                    data=df,
+                    orient="h", size=2, aspect=3.5, palette="Set3",
+                    kind="violin", split=True, cut=0, bw=.2)
+plt.show()
+
+
+# data.to_csv("data_clean.csv", index=False, header=False)
